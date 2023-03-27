a610dad48514d278c448f8044efc33cdca3b10ac
Nicholas Stevick <nicholasstevick23@gmail.com>
2023-03-27 11:06:15 -0400

    Added Jwt and Jpa

---
+++ MarketPlace/src/main/java/com/Telescope/Repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.Telescope.Repository;
+
+import com.Telescope.Models.ERole;
+import com.Telescope.Models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(ERole name);
+}
